9b120486ca6d051489cdcfb38807c0256ab7eb88
takealook2 <a1vm5h@gmail.com>
2024-09-10 11:47:24 +0900

    1181 : 단어 정렬(람다식 사용)

--- W01/W01-15-1181.py
+++ W01/W01-15-1181.py
@@ -12,6 +12,9 @@ for i in range(num):
 arr_unique = list(set(arr))
 
 arr_unique.sort()
+# lambda 함수를 통해 (단어의 길이, 단어) 식으로 튜플을 형성함
+# 튜플을 형성한 후 일단 단어의 길이를 가지고 정렬하고
+# 단어의 길이를 정렬한 후에 단어를 가지고 오름차순 정렬을 함
 arr_unique.sort(key=lambda x: len(x))
 
 
